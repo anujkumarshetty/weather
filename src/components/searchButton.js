ab2989900f691b6f88faa9bc5609ac9e08bd42d0
Anuj kumar <anujkumar.atma@gmail.com>
2019-01-15 00:33:02 +0530

    Error handler implementation

--- src/components/searchButton.js
+++ src/components/searchButton.js
@@ -4,6 +4,11 @@ import { withStyles } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 import Search from "@material-ui/icons/Search";
 import axios from "axios";
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
 
 import WeatherDetail from "./weatherDetail";
 import RecentSearch from "./recentSearch";
@@ -31,36 +36,58 @@ const styles = theme => ({
     position: "absolute",
     marginLeft: "50%",
     marginTop: "10%"
-  }
+  },
+   close: {
+    padding: theme.spacing.unit / 2,
+  },
 });
 
 class SearchButton extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
       weatherData: {},
       loader: false,
-      searchResult: []
+      searchResult: [],
+      open: false
     };
   }
+
+  handleClick = () => {
+    this.setState({ open: true });
+    setTimeout(this.handleClose, 3000)
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
+  };
 
   handleChange = event => {
     console.log(this.props.location);
     this.setState({ loader: true });
     axios.get(`http://localhost:8000/city/${this.props.location}`).then(res => {
       console.log(res.data[0]);
-
+    if(res.data.length>0){
       axios
-        .get(`http://localhost:8000/woeid/${res.data[0].woeid}`)
-        .then(res => {
-          console.log(res.data);
-          this.setState({ weatherData: res.data });
-          this.setState({
-            searchResult: [...this.state.searchResult, res.data]
-          });
-          console.log(this.state.searchResult);
-          this.setState({ loader: false });
+      .get(`http://localhost:8000/woeid/${res.data[0].woeid}`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ weatherData: res.data });
+        this.setState({
+          searchResult: [...this.state.searchResult, res.data]
         });
+        console.log(this.state.searchResult);
+        this.setState({ loader: false });
+      });
+    }else{
+      this.handleClick();
+      this.setState({ loader: false });
+    }
     });
   };
 
@@ -99,6 +126,33 @@ class SearchButton extends Component {
             this.state.searchResult ? this.state.searchResult : []
           }
         />
+        <div>
+        {/* <Button onClick={this.handleClick}>Open simple snackbar</Button> */}
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={this.state.open}
+          autoHideDuration={6000}
+          onClose={this.handleClose}
+          ContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          message={<span id="message-id">Oops!! cannot find data for <span style={{color:'red'}}>  {this.props.location}</span>, Try rephrasing. </span>}
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              className={classes.close}
+              onClick={this.handleClose}
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
+      </div>
       </div>
     );
   }
